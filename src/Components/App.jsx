26422e35e3406d5e8779d650a5fcd0197fc27f55
koshmaniuk <trident93@gmail.com>
2024-02-12 23:00:41 -0500

    all done, without css modules

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,105 +1,119 @@
-import { useState, useId } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
-import * as Yup from 'yup';
-// import { ContactList } from './ContactList/ContactList';
-// import { SearchBox } from './SearchBox/SearchBox';
-// import { ContactForm } from './ContactForm/ContactForm';
-// const App = () => {
-//   const [filter, setFilter] = useState('');
-//   const [contacts, setContacts] = useState([
-//     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//   ]);
 
-//   const filteredContacts = contacts.filter(contact =>
-//     contact.name.toLowerCase().includes(filter.toLowerCase())
-//   );
+import { ContactList } from './ContactList/ContactList';
+import { SearchBox } from './SearchBox/SearchBox';
+import { ContactForm } from './ContactForm/ContactForm';
 
-//   // const addUser = () => {};
-//   const deleteUser = userId => {
-//     setContacts(prevContacts => {
-//       return prevContacts.filter(prevContact => prevContact.id !== userId);
-//     });
-//   };
+const App = () => {
+  const [filter, setFilter] = useState('');
+  // const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState(() => {
+    const contactsFromLs = window.localStorage.getItem('saved-contacts');
+    if (contactsFromLs !== null) {
+      return JSON.parse(contactsFromLs);
+    }
+    return [];
+  });
 
-//   return (
-//     <div>
-//       <h1>Phonebook</h1>
-//       <ContactForm />
-//       <SearchBox value={filter} onChange={setFilter} />
-//       <ContactList items={filteredContacts} onDelete={deleteUser} />
-//     </div>
-//   );
-// };
+  // const [reviews, setReviews] = useState(() => {
+  //   const reviewsFromLs = window.localStorage.getItem('saved-reviews');
+  //   if (reviewsFromLs !== null) {
+  //     return JSON.parse(reviewsFromLs);
+  //   }
+  //   return { good: 0, neutral: 0, bad: 0 };
+  // });
 
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
-const FeedbackSchema = Yup.object().shape({
-  username: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Required'),
-  email: Yup.string().email('Must be a valid email!').required('Required'),
-  message: Yup.string().min(3, 'Too short').max(256, 'Too long').required('Required'),
-  level: Yup.string().oneOf(['good', 'neutral', 'bad']).required('Required'),
-});
-
-const initialValues = {
-  username: '',
-  email: '',
-  message: '',
-  level: 'good',
-};
-
-const FeedbackForm = () => {
-  const nameFieldId = useId();
-  const emailFieldId = useId();
-  const msgFieldId = useId();
-  const levelFieldId = useId();
-
-  const handleSubmit = (values, actions) => {
-    console.log(values);
-    actions.resetForm();
+  const addContact = newContact => {
+    console.log(newContact);
+    setContacts(prevContacts => {
+      return [...prevContacts, newContact];
+    });
+  };
+  const deleteUser = userId => {
+    setContacts(prevContacts => {
+      return prevContacts.filter(prevContact => prevContact.id !== userId);
+    });
   };
 
+  useEffect(() => {
+    window.localStorage.setItem('saved-contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
-    <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={FeedbackSchema}>
-      <Form>
-        <div>
-          <label htmlFor={nameFieldId}>Username</label>
-          <Field type="text" name="username" id={nameFieldId} />
-          <ErrorMessage name="username" as="span" />
-        </div>
-
-        <div>
-          <label htmlFor={emailFieldId}>Email</label>
-          <Field type="email" name="email" id={emailFieldId} />
-          <ErrorMessage name="email" as="span" />
-        </div>
-
-        <div>
-          <label htmlFor={msgFieldId}>Message</label>
-          <Field as="textarea" name="message" id={msgFieldId} rows="5" />
-          <ErrorMessage name="message" as="span" />
-        </div>
-
-        <div>
-          <label htmlFor={levelFieldId}>Service satisfaction level</label>
-          <Field as="select" name="level" id={levelFieldId}>
-            <option value="good">Good</option>
-            <option value="neutral">Neutral</option>
-            <option value="bad">Bad</option>
-          </Field>
-          <ErrorMessage name="level" as="span" />
-        </div>
-
-        <button type="submit">Submit</button>
-      </Form>
-    </Formik>
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onAdd={addContact} />
+      <SearchBox value={filter} onChange={setFilter} />
+      <ContactList items={filteredContacts} onDelete={deleteUser} />
+    </div>
   );
 };
 
-const App = () => {
-  return <FeedbackForm />;
-};
+// const FeedbackSchema = Yup.object().shape({
+//   username: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Required'),
+//   email: Yup.string().email('Must be a valid email!').required('Required'),
+//   message: Yup.string().min(3, 'Too short').max(256, 'Too long').required('Required'),
+//   level: Yup.string().oneOf(['good', 'neutral', 'bad']).required('Required'),
+// });
+
+// const initialValues = {
+//   username: '',
+//   email: '',
+//   message: '',
+//   level: 'good',
+// };
+
+// const FeedbackForm = () => {
+//   const nameFieldId = useId();
+//   const emailFieldId = useId();
+//   const msgFieldId = useId();
+//   const levelFieldId = useId();
+
+//   const handleSubmit = (values, actions) => {
+//     console.log(values);
+//     actions.resetForm();
+//   };
+
+//   return (
+//     <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={FeedbackSchema}>
+//       <Form>
+//         <div>
+//           <label htmlFor={nameFieldId}>Username</label>
+//           <Field type="text" name="username" id={nameFieldId} />
+//           <ErrorMessage name="username" as="span" />
+//         </div>
+
+//         <div>
+//           <label htmlFor={emailFieldId}>Email</label>
+//           <Field type="email" name="email" id={emailFieldId} />
+//           <ErrorMessage name="email" as="span" />
+//         </div>
+
+//         <div>
+//           <label htmlFor={msgFieldId}>Message</label>
+//           <Field as="textarea" name="message" id={msgFieldId} rows="5" />
+//           <ErrorMessage name="message" as="span" />
+//         </div>
+
+//         <div>
+//           <label htmlFor={levelFieldId}>Service satisfaction level</label>
+//           <Field as="select" name="level" id={levelFieldId}>
+//             <option value="good">Good</option>
+//             <option value="neutral">Neutral</option>
+//             <option value="bad">Bad</option>
+//           </Field>
+//           <ErrorMessage name="level" as="span" />
+//         </div>
+
+//         <button type="submit">Submit</button>
+//       </Form>
+//     </Formik>
+//   );
+// };
 
 export default App;
--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -1,11 +1,41 @@
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { useId } from 'react';
+import * as Yup from 'yup';
+import { nanoid } from 'nanoid';
 
-export const ContactForm = () => {
+const ContactsSchema = Yup.object().shape({
+  name: Yup.string().min(3, 'Too short!').max(50, 'Too long!').required('Required'),
+  number: Yup.string().min(3, 'Too short!').max(50, 'Too long!').required('Required'),
+});
+
+const initialValues = {
+  name: '',
+  number: '',
+};
+
+export const ContactForm = ({ onAdd }) => {
+  const nameFieldId = useId();
+  const numberFieldId = useId();
+
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
+    onAdd({ id: nanoid(), ...values });
+  };
+
   return (
-    <Formik initialValues={{}} onSubmit={() => {}}>
+    <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={ContactsSchema}>
       <Form>
-        <Field type="text" name="username" />
-        <Field type="text" name="number" />
+        <div>
+          <label htmlFor={nameFieldId}>Name</label>
+          <Field type="text" name="name" id={nameFieldId} />
+          <ErrorMessage name="name" component="span" />
+        </div>
+        <div>
+          <label htmlFor={numberFieldId}>Number</label>
+          <Field type="text" name="number" id={numberFieldId} />
+          <ErrorMessage name="number" component="span" />
+        </div>
         <button type="submit">Add contact</button>
       </Form>
     </Formik>
